ddcabdf5af22427e87ad91636958fbf417a82b04
ignitedevv <info@ignitedevv.com>
2023-04-18 14:21:38 -0400

    t

--- mMoney/asgi.py
+++ mMoney/asgi.py
@@ -1,16 +1,17 @@
-"""
-ASGI config for mMoney project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
-"""
-
 import os
-
 from django.core.asgi import get_asgi_application
+from django.urls import path
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mMoney.settings')
+import routing
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'yourproject.settings')
 
-application = get_asgi_application()
+application = ProtocolTypeRouter({
+    "http": get_asgi_application(),
+    "websocket": AuthMiddlewareStack(
+        URLRouter(
+            routing.websocket_urlpatterns
+        )
+    ),
+})
--- routing.py
+++ routing.py
@@ -1,9 +1,7 @@
-from channels.routing import ProtocolTypeRouter, URLRouter
 from django.urls import path
+
 from budget import consumers
 
-application = ProtocolTypeRouter({
-    'websocket': URLRouter([
-        path('ws/chat/<str:room_name>/', consumers.ChatConsumer.as_asgi()),
-    ]),
-})
+websocket_urlpatterns = [
+    path('ws/chat/<str:room_name>/', consumers.ChatConsumer.as_asgi()),
+]
