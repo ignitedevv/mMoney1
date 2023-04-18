ba51ba85ddf25985b1da52626ba3c7f13f7f77e6
ignitedevv <info@ignitedevv.com>
2023-04-18 14:30:46 -0400

    Update asgi.py

--- mMoney/asgi.py
+++ mMoney/asgi.py
@@ -5,7 +5,7 @@ from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 
 import routing
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'yourproject.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mMoney.settings')
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
