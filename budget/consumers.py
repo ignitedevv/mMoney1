177a8ed4b0b5b67321d2b854dfcc8f70b5c2717e
ignitedevv <info@ignitedevv.com>
2023-04-20 02:02:30 -0400

    test

--- budget/consumers.py
+++ budget/consumers.py
@@ -2,33 +2,18 @@ import json
 from channels.generic.websocket import AsyncWebsocketConsumer
 
 class VideoCallConsumer(AsyncWebsocketConsumer):
-    peers = set()
+    users = set()
 
     async def connect(self):
         await self.accept()
-        self.peers.add(self)
+        VideoCallConsumer.users.add(self)
 
     async def disconnect(self, close_code):
-        if hasattr(self, 'peer_id'):
-            for peer in self.peers:
-                if peer != self:
-                    await peer.send(text_data=json.dumps({
-                        'type': 'leave',
-                        'peerId': self.peer_id,
-                    }))
-        self.peers.remove(self)
+        VideoCallConsumer.users.remove(self)
 
     async def receive(self, text_data):
         data = json.loads(text_data)
-        message_type = data['type']
-
-        if message_type == 'join':
-            peer_id = data['peerId']
-            self.peer_id = peer_id
-
-            for peer in self.peers:
-                if peer != self:
-                    await peer.send(text_data=json.dumps({
-                        'type': 'offer',
-                        'peerId': peer_id,
-                    }))
+        if data["type"] == "join":
+            for user in VideoCallConsumer.users:
+                if user != self:
+                    await user.send(json.dumps({"type": "offer", "peerId": data["peerId"]}))
