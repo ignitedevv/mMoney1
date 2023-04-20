583f689a170cab4905764f446ee127e52e229b03
ignitedevv <info@ignitedevv.com>
2023-04-20 01:50:05 -0400

    test

--- routing.py
+++ routing.py
@@ -8,7 +8,7 @@ application = ProtocolTypeRouter(
         "http": get_asgi_application(),
         "websocket": URLRouter(
             [
-                path("budget/video_call/", consumers.VideoCallConsumer.as_asgi()),
+                path("ws/budget/video_call/", consumers.VideoCallConsumer.as_asgi()),
             ]
         ),
     }
