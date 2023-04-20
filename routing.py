93e617ea77c524daaf1df353dbf3feb5d9a2f5b5
ignitedevv <info@ignitedevv.com>
2023-04-20 01:56:14 -0400

    test

--- routing.py
+++ routing.py
@@ -1,15 +1,6 @@
-from channels.routing import ProtocolTypeRouter, URLRouter
 from django.urls import path
 from budget import consumers
-from django.core.asgi import get_asgi_application
 
-application = ProtocolTypeRouter(
-    {
-        "http": get_asgi_application(),
-        "websocket": URLRouter(
-            [
-                path("/ws/budget/video_call/", consumers.VideoCallConsumer.as_asgi()),
-            ]
-        ),
-    }
-)
+websocket_urlpatterns = [
+    path("ws/budget/video_call/", consumers.VideoCallConsumer.as_asgi()),
+]
