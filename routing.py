d5c0804860cf4f719a833fa9dd8700b9a738a209
ignitedevv <info@ignitedevv.com>
2023-04-20 02:40:20 -0400

    test

--- routing.py
+++ routing.py
@@ -2,5 +2,5 @@ from django.urls import path
 from budget import consumers
 
 websocket_urlpatterns = [
-    path("/budget/video_call/test/", consumers.VideoCallConsumer.as_asgi()),
+    path("/budget/video_call/", consumers.VideoCallConsumer.as_asgi()),
 ]
